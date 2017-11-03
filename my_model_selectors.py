85fb434b240ed620cc105add52e272a3a15ba003
Murat Senel <msenel@gmail.com>
2017-11-03 11:15:34 -0400

    Submitted version

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -1,6 +1,7 @@
 import math
 import statistics
 import warnings
+import logging
 
 import numpy as np
 from hmmlearn.hmm import GaussianHMM
@@ -67,6 +68,8 @@ class SelectorBIC(ModelSelector):
     http://www2.imm.dtu.dk/courses/02433/doc/ch6_slides.pdf
     Bayesian information criteria: BIC = -2 * logL + p * logN
     """
+    # The source code is adapted from https://github.com/sumitbinnani/Sign-Language-Recognizer
+    # and https://github.com/diegoalejogm/AI-Nanodegree/tree/master/sign-language-recognizer
 
     def select(self):
         """ select the best model for self.this_word based on
@@ -76,8 +79,39 @@ class SelectorBIC(ModelSelector):
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        # Implement model selection based on BIC scores
+
+        # Initialization of the parameters for tracking
+        # best score and model
+        best_score , best_model = math.inf , None
+
+        # Iterate over states (n_components)
+        for n_components in range(self.min_n_components, self.max_n_components+1):
+            try:
+                # GaussianHMM with n_compotents states
+                tmp_model = self.base_model(n_components)
+
+                # BIC = -2 * logL + p * logN
+                logL = tmp_model.score(self.X, self.lengths)
+                n_features = self.X.shape[1]
+
+                # p value in BIC
+                n_params = n_components * (n_components - 1) + 2 * n_features * n_components
+
+                logN = np.log(self.X.shape[0])
+
+                # Calculate BIC score
+                bic_score = -2 * logL + n_params * logN
+
+                # Best BIC score so far
+                if bic_score < best_score:
+                    best_score, best_model = bic_score, tmp_model
+
+            except Exception as e:
+                logging.info("Exception:",self.this_word, e)
+                continue
+
+        return best_model if best_model is not None else self.base_model(self.n_constant)
 
 
 class SelectorDIC(ModelSelector):
@@ -89,21 +123,148 @@ class SelectorDIC(ModelSelector):
     https://pdfs.semanticscholar.org/ed3d/7c4a5f607201f3848d4c02dd9ba17c791fc2.pdf
     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
     '''
+    # The source code is adapted from https://github.com/sumitbinnani/Sign-Language-Recognizer
+    # and https://github.com/diegoalejogm/AI-Nanodegree/tree/master/sign-language-recognizer
+
+    _logL_values = None
+
+    def log_likelihood_terms(self):
+
+        values = {}
+
+        for n_components in range(self.min_n_components, self.max_n_components+1):
+
+            # Calculate logL for all words with n_components
+            n_components_logL = {}
+            for word in self.words.keys():
+                X_word, lengths_word = self.hwords[word]
+                try:
+                    # GaussianHMM with n_compotents states
+                    model = GaussianHMM(n_components=n_components, n_iter=1000).fit(X_word, lengths_word)
+
+                    # Log-Likelihood Term
+                    n_components_logL[word] = model.score(X_word, lengths_word)
+
+                except Exception as e:
+                    logging.info("Exception:", e)
+                    continue
+
+            values[n_components] = n_components_logL
+
+        return (values)
+
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
+        # Implement model selection based on DIC scores
 
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+
+        # Initialization of the parameters for tracking
+        best_score, best_n_components = -math.inf, None
+
+        # log Likehood values inside SUM(log(P(X(all but i)
+        if(self._logL_values == None):
+            self._logL_values = self.log_likelihood_terms()
+
+        logL_values = self._logL_values
+        for n_components in range(self.min_n_components, self.max_n_components+1):
+            
+            # Calculate logL for all words with n_components
+            data_likehood = logL_values[n_components]
+
+            # Skip current n_components if self.this_word doesn't have a valid model
+            if(self.this_word not in data_likehood):
+                logging.info("{} does not have a valid model",format(self.this_word))
+                continue
+            
+            # Avg. of Anti-likelihood terms (i != j)
+            avg = np.mean([data_likehood[word] for word in data_likehood.keys() if word != self.this_word])
+
+            # DIC Score
+            DIC = data_likehood[self.this_word] - avg
+            
+            # Is this the best score so far?
+            if(best_score is None or DIC > best_score):
+                best_score, best_n_components = DIC, n_components
+
+        # If could not find a best_score
+        if(best_score == None):
+            best_n_components = self.n_constant
+
+        return self.base_model(best_n_components)
 
 
 class SelectorCV(ModelSelector):
     ''' select best model based on average log Likelihood of cross-validation folds
 
     '''
+    # The source code is adapted from https://github.com/sumitbinnani/Sign-Language-Recognizer
+    # and https://github.com/diegoalejogm/AI-Nanodegree/tree/master/sign-language-recognizer
+
+    n_splits = 3
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection using CV
-        raise NotImplementedError
+        # Implement model selection using CV
+
+        # Initialization of the parameters for tracking
+        # best score and model
+        best_score , best_model = -math.inf , None
+
+        # Iterate over states (n_components)
+        for n_components in range(self.min_n_components, self.max_n_components+1):
+            # Init in the loop
+            tmp_scores = []
+            tmp_model = None
+            logL = None
+
+            if len(self.sequences) >= self.n_splits:
+                # K-Folds cross-validator
+                split_method = KFold(random_state=self.random_state, n_splits=self.n_splits)
+
+                for train, test in split_method.split(self.sequences):
+                    X_train , lengths_train = combine_sequences(train, self.sequences)
+                    X_test , lengths_test = combine_sequences(test, self.sequences)
+
+                    try:
+                        # Hidden Markov Model with Gaussian emissions.
+                        tmp_model = GaussianHMM(n_components=n_components,
+                                                covariance_type="diag", n_iter=1000,
+                                                verbose=False).fit(X_train, lengths_train)
+
+                        # Compute the log probability under the model.
+                        logL = tmp_model.score(X_test, lengths_test)
+
+                        tmp_scores.append(logL)
+
+                    except Exception as e:
+                        logging.info("Exception:",self.this_word, e)
+                        break
+
+                    # average log Likelihood score
+                    avg_score = np.average(tmp_scores) if len(tmp_scores) > 0 else (-math.inf)
+
+                    # Is this the best score so far?
+                    if avg_score > best_score:
+                        best_score, best_model = avg_score, tmp_model
+
+            else:   # len(self.sequences) < self.n_splits
+                logging.info('Number of sequences less than splits\n')
+                try:
+                    # GaussianHMM with n_compotents states
+                    tmp_model = self.base_model(n_components)
+
+                    # Compute the log probability under the model.
+                    logL = tmp_model.score(self.X, self.lengths)
+
+                    # Is this the best score so far?
+                    if logL > best_score:
+                        best_score, best_model = logL, tmp_model
+
+                except Exception as e:
+                        logging.info("Exception:",self.this_word, e)
+                        break
+
+
+        return best_model if best_model is not None else self.base_model(self.n_constant)
--- my_recognizer.py
+++ my_recognizer.py
@@ -1,4 +1,6 @@
+import math
 import warnings
+import logging
 from asl_data import SinglesData
 
 
@@ -17,9 +19,37 @@ def recognize(models: dict, test_set: SinglesData):
        guesses is a list of the best guess words ordered by the test set word_id
            ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
    """
+    # The source code is adapted from https://github.com/sumitbinnani/Sign-Language-Recognizer
+    # and https://github.com/diegoalejogm/AI-Nanodegree/tree/master/sign-language-recognizer
+
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+    
+    # Implement the recognizer
+    for item in range(test_set.num_items):
+        
+      best_prob, best_word = -math.inf, None
+      tmp_word_probabilities = {}
+        
+      # Get current test set sequences
+      tmp_sequences, tmp_lengths = test_set.get_item_Xlengths(item)
+        
+      for word, model in models.items():
+        try:
+
+          # Test scores
+          tmp_word_probabilities[word] = model.score(tmp_sequences, tmp_lengths)
+            
+        except Exception as e:
+          logging.info("Exception:", e)
+          tmp_word_probabilities[word] = -math.inf
+            
+        # Best probability so far?
+        if(tmp_word_probabilities[word] > best_prob):
+          best_prob, best_word = tmp_word_probabilities[word], word
+
+      probabilities.append(tmp_word_probabilities)
+      guesses.append(best_word)
+
+    return probabilities , guesses
